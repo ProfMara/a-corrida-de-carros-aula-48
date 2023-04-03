93f022d27cf09fbf78977edd47bef7273a99a45f
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-04-03 18:57:17 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -108,13 +108,11 @@ class Game{
         //cria a sprite do carro1
         car1 = createSprite(width/2 - 100, height-100);
         car1.addImage("carro", carimg1);
-        car1.addImage("explosao", blastImg);
         car1.scale = 0.07;
 
         //cria a sprite do carro2
         car2 = createSprite(width/2 + 100, height-100);
         car2.addImage("carro", carimg2);
-        car2.addImage("explosao", blastImg);
         car2.scale = 0.07;
 
         //adiciona as duas sprites na matriz cars
